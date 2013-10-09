98d55fc7def3eb749892c1570bb20bb1625ebca4
mumair85 <m_umair_85@hotmail.com>
2013-10-09 20:08:08 +0100

    Updated folder name and changed namespace

--- uGen.Extensions/Extensions/StringExtensions.cs
+++ uGen.Extensions/Extensions/StringExtensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Extensions
+namespace uGen.Extensions
 {
     public static class StringExtensions
     {
