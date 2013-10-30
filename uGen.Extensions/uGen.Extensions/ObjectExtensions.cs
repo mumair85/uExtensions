5800343f7ad912cc03f68acdd414886c6077a81d
mumair85 <m_umair_85@hotmail.com>
2013-10-30 14:18:06 +0000

    Added new extensions

---
+++ uGen.Extensions/uGen.Extensions/ObjectExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uGen.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Returns true if the list does not contain the passed parameter(s)
+        /// From: http://stackoverflow.com/a/833477/1158845
+        /// </summary>
+        /// <param name="source">the object to search in</param>
+        /// <param name="list">the list of items to search for</param>
+        /// <returns>true if the object is not in the list passed</returns>
+        public static bool NotContains<T>(this T source, params T[] list)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            return !list.Contains(source);
+        }
+    }
+}
--- uGen.Extensions/uGen.Extensions/StringExtensions.cs
+++ uGen.Extensions/uGen.Extensions/StringExtensions.cs
@@ -128,5 +128,39 @@ namespace uGen.Extensions
             }
             return "";
         }
+
+        /// <summary>
+        /// Indicates whether the current string matches the supplied wildcard pattern.
+        /// From: http://stackoverflow.com/a/3834758/1158845
+        /// </summary>
+        /// <param name="s">The string instance where the extension method is called</param>
+        /// <param name="wildcardPattern">The wildcard pattern to match.  Syntax matches VB's Like operator.</param>
+        /// <returns>true if the string matches the supplied pattern, false otherwise.</returns>
+        /// <remarks>See http://msdn.microsoft.com/en-us/library/swf8kaxw(v=VS.100).aspx</remarks>
+        public static bool IsLike(this string s, string wildcardPattern)
+        {
+            if (s == null || String.IsNullOrEmpty(wildcardPattern)) return false;
+            // turn into regex pattern, and match the whole string with ^$
+            var regexPattern = "^" + Regex.Escape(wildcardPattern) + "$";
+
+            // add support for ?, #, *, [], and [!]
+            regexPattern = regexPattern.Replace(@"\[!", "[^")
+                                       .Replace(@"\[", "[")
+                                       .Replace(@"\]", "]")
+                                       .Replace(@"\?", ".")
+                                       .Replace(@"\*", ".*")
+                                       .Replace(@"\#", @"\d");
+
+            bool result;
+            try
+            {
+                result = Regex.IsMatch(s, regexPattern);
+            }
+            catch (ArgumentException ex)
+            {
+                throw new ArgumentException(String.Format("Invalid pattern: {0}", wildcardPattern), ex);
+            }
+            return result;
+        }
     }
 }
